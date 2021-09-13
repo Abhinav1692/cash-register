68d8d2303f331d06390d22b1ee4942081acb21b5
Abhinav1692 <89593847+Abhinav1692@users.noreply.github.com>
2021-09-13 23:34:38 +0530

    css part added

---
+++ index.js
@@ -0,0 +1,5 @@
+var billAmount=document.querySelector("#bill-amount");
+var cashAmount=document.querySelector("#cash-given");
+var checkButton=document.querySelector("#check-button");
+var errorMessage=document.querySelector("#error-msg");
+
