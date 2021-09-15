6a828dc99c64e986a20b9a14e968ec69d0257993
Abhinav1692 <89593847+Abhinav1692@users.noreply.github.com>
2021-09-15 10:58:16 +0530

    Revert "Logic added"
    This reverts commit 54ba5beb131a701469a90fa2f5d12d6fc4ce9d56.

--- index.js
+++
@@ -1,4 +0,0 @@
-var billAmount=document.querySelector("#bill-amount");
-var cashAmount=document.querySelector("#cash-given");
-var checkButton=document.querySelector("#check-button");
-var errorMessage=document.querySelector("#error-msg");
